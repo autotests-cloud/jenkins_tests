e04b5838ee300147ce76c9d2c323b76cf90a7d3e
Vasenkov Stanislav <aanher@gmail.com>
2020-08-27 20:10:34 +0300

    Create GoogletTests.java
    added tests

---
+++ src/test/java/tests/GoogletTests.java
@@ -0,0 +1,29 @@
+package tests;
+
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+
+
+class GoogleTests {
+
+    @Test
+    void selenideSearchTest() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+
+        // Открыть google
+        open("https://google.com");
+
+        // Ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter(); // кнопка логина
+
+        // Проверить, что Selenide появился в результатах поиска
+        $(".search-results").shouldHave(text("ru.selenide.org"));
+    }
+
+}
