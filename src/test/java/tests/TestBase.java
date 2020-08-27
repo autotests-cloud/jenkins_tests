f5aed3b75adc82cac11ae8dcc3229265474e5593
svasenkov <aanher@gmail.com>
2020-08-27 20:58:36 +0300

    added more attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeAll;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
-import static org.openqa.selenium.logging.LogType.BROWSER;
+import static org.openqa.selenium.logging.LogType.*;
 
 
 class TestBase {
@@ -27,12 +27,32 @@ class TestBase {
     public void afterEach(){
         attachScreenshot("Last screenshot");
         attachPageSource();
-        attachAsText("Browser console logs", getConsoleLogs());
+        attachAsText("getBrowserConsoleLogs console logs", getBrowserConsoleLogs());
+        attachAsText("getClientConsoleLogs console logs", getClientConsoleLogs());
+        attachAsText("getDriverConsoleLogs console logs", getDriverConsoleLogs());
+        attachAsText("getPerformanceConsoleLogs console logs", getPerformanceConsoleLogs());
+        attachAsText("getProfilerConsoleLogs console logs", getProfilerConsoleLogs());
 
         closeWebDriver();
     }
 
-    public static String getConsoleLogs() {
+    public static String getBrowserConsoleLogs() {
         return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
+    }
+
+    public static String getClientConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(CLIENT));
+    }
+
+    public static String getDriverConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(DRIVER));
+    }
+
+    public static String getPerformanceConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(PERFORMANCE));
+    }
+
+    public static String getProfilerConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(PROFILER));
     }
 }
